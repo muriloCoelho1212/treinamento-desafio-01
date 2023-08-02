bf3652b466b402557a1ffaa0a53900e0b76c7227
Murilo <m.coelhos1212@gmail.com>
2023-08-02 15:01:25 -0300

    estilizando o site e iniciando o js

--- treinamento-desafio-01/script.js
+++ treinamento-desafio-01/script.js
@@ -1,6 +1,15 @@
-function cadastro(){
-    var nome = document.getElementById('#nome')
-    var nomeTabela = document.getElementById('#nomeTabela')
-
-    nomeTabela.innerHTML = nome
+function modalCadastro(){
+    let modal = document.getElementById('modal-cadastro')
+    let span = document.getElementById('close')
+    
+    modal.style.display = "block"
+    
+    span.onclick = function(){
+        modal.style.display = "none"
+    }
+    // window.onclick = function(event) {
+    //     if(event.target == modal){
+    //         modal.style.display = "none"
+    //     }
+    // }
 }
