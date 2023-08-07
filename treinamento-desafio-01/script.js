7e3a6806f15793095d143dc94c62b63f6b6aca21
Murilo <m.coelhos1212@gmail.com>
2023-08-07 12:30:56 -0300

    Criando linhas na tabela
    Criei a função mostraCadastro() para mostrar mostrar os dados na tabela; Criei a função criaLinha(), que insere uma nova linha com 6 células para cada dado na tabela; Deletei a classe das células e as selecionei usando o nome da tag no CSS;

--- treinamento-desafio-01/script.js
+++ treinamento-desafio-01/script.js
@@ -3,14 +3,17 @@
 const modal = document.querySelector('#modal-cadastro')
 const btnAbrirModal = document.querySelector('#botao-abrir-modal')
 const btnFecharModal = document.querySelector('#botao-fechar-modal')
+const tabela = document.querySelector("table")
 
 btnAbrirModal.addEventListener('click', () => {
     modal.style.display = "block"
 })
 
-btnFecharModal.addEventListener('click', () => {
+btnFecharModal.addEventListener('click', closeModal)
+
+function closeModal(){
     modal.style.display = "none"
-})
+}
 
 // Função criar cadastro
 
@@ -21,12 +24,24 @@ btnCadastrar.addEventListener('click', () => {
 })
 
 function criarCadastro(){
+
     let nome = document.querySelector('#nome-cadastro')
-    let data = document.querySelector('#data-cadastro')
+
+    let data = new Date(document.querySelector('#data-cadastro').value)
+    let formatData = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'})
+
     let sexo = document.querySelector('#sexo-cadastro')
     let endereco = document.querySelector('#endereco-cadastro')
     let tel = document.querySelector('#tel-cadastro')
     let email = document.querySelector('#email-cadastro')
+
+    closeModal()
+    mostraCadastro(nome.value, formatData, sexo.value, endereco.value, tel.value, email.value)
+}
+
+// Função mostrar cadastro
+
+function mostraCadastro(nome, formatData, sexo, endereco, tel, email){
 
     let nomeTabela = document.querySelector('#nomeTabela')
     let dataTabela = document.querySelector('#dataTabela')
@@ -34,11 +49,25 @@ function criarCadastro(){
     let enderecoTabela = document.querySelector('#enderecoTabela')
     let telTabela = document.querySelector('#celularTabela')
     let emailTabela = document.querySelector('#emailTabela')
+    
+    nomeTabela.innerText = nome
+    dataTabela.innerText = formatData
+    sexoTabela.innerText = sexo
+    enderecoTabela.innerText = endereco
+    telTabela.innerText = tel
+    emailTabela.innerText = email
 
-    nomeTabela.innerText = nome.value
-    dataTabela.innerText = data.value
-    sexoTabela.innerText = sexo.value
-    enderecoTabela.innerText = endereco.value
-    telTabela.innerText = tel.value
-    emailTabela.innerText = email.value
+    criaLinha(nome, formatData, sexo, endereco, tel, email)
+}
+
+function criaLinha(nome, formatData, sexo, endereco, tel, email){
+    let numLinha = tabela.rows.length
+    let novaLinha = tabela.insertRow(1)
+    novaLinha.insertCell(0).innerText = nome
+    novaLinha.insertCell(1).innerText = formatData
+    novaLinha.insertCell(2).innerText = sexo
+    novaLinha.insertCell(3).innerText = endereco
+    novaLinha.insertCell(4).innerText = tel
+    novaLinha.insertCell(5).innerText = email
+    console.log(numLinha)
 }
